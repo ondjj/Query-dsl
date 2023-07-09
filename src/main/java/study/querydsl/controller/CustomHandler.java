0cd0e79fe818021c5577199d0e76f522501c82e2
ondjj <jcjk0302@gmail.com>
2023-07-09 21:08:40 +0900

    dsl

---
+++ src/main/java/study/querydsl/controller/CustomHandler.java
@@ -0,0 +1,13 @@
+package study.querydsl.controller;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class CustomHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
